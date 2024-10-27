d3446c12079dbc4927b7eae70328e0d3732cb962
Vasco Costa <vascomacosta@gmail.com>
2024-10-27 18:45:28 +0000

    Replace form border with zero margin and padding

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,7 @@ async fn root() -> Markup {
                         h1 class="text-warning-emphasis" { "🎬 Gluon Open Movie Database" }
                         div class="htmx-indicator" { p class="text-success-emphasis" { "searching..." } }
                         form
-                            class="form border border-primary"
+                            class="form m-0 p-0"
                             hx-get="/search"
                             hx-target="#results"
                             hx-trigger="input changed delay:500ms, search"
